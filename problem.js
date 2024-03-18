087d3ed6b04927533ec05e287773b0a296556880
alhaj sarkar <you@domain.com>
2024-03-18 16:18:40 +0600

    slice method added

--- problem.js
+++ problem.js
@@ -1,15 +1,15 @@
-var x = 5;
-var y = 8;
-var item = (x++ - ++y + x++);
-console.log(item);
+// var x = 5;
+// var y = 8;
+// var item = (x++ - ++y + x++);
+// console.log(item);
 
-var a = '99';
-var b = '101';
-var items = (a<+b);
-console.log(items) 
+// var a = '99';
+// var b = '101';
+// var items = (a<+b);
+// console.log(items) 
 
 
-var numArray = [10,20,30,40,50,60];
+var countNumber = [10,20,30,40,50,60,70];
 
-var toTal= numArray.push(66);
+var toTal= countNumber.push(99);
  console.log('Number', toTal);
